491abbcbea16932887ee7d542444b2f498263db9
didierDefrance <didier@d-defrance.fr>
2022-02-07 18:55:44 +0100

    web

---
+++ tp-js/web/js/calcul.spec.js
@@ -0,0 +1,29 @@
+describe("my calculator tests", function () {
+    var detector;
+	var compteur=0;
+
+    beforeEach(function () {
+     console.log("initialisation : new ... or ..." );
+	 detector = 0;
+	 compteur++;
+    });
+	
+
+    it("2+3==5?", function () {
+         expect(calculerOp('+',2,3)).toBe(5);
+    });
+    it("2*3==6?", function () {
+        expect(calculerOp('*',2,3)).toBe(6);
+    });
+	it("8-5==3?", function () {
+        expect(calculerOp('-',8,5)).toBe(3);
+    });
+	it("8/4==2?", function () {
+         expect(calculerOp('/',8,4)).toBe(2);
+    });
+	
+	afterAll( function(){
+		expect(compteur).toBe(4);
+	});
+            
+});
