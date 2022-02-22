66b19b19d35dd29ef921013b571093ca0b51c500
didierDefrance <didier@d-defrance.fr>
2022-02-22 16:27:00 +0100

    avec menu deroulant

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component,  Input,  OnInit } from '@angular/core';
+import { MenuDefinition } from 'src/bs-util/data/MenuDefinition';
 import { PreferencesService } from '../common/service/preferences.service';
 
 @Component({
@@ -10,6 +11,19 @@ export class HeaderComponent implements OnInit {
 
   @Input()
   titre : string = "titre_par_defaut";
+
+  //<bsu-nav-bar  [menuDefs]="myMenuDefs"></bsu-nav-bar> coté .html
+  myMenuDefs : MenuDefinition[] = [
+    { label : "basic" , path : "/ngr-basic" } , 
+    { label : "welcome" , path : "/ngr-welcome" } ,
+    { label : "autres" , 
+      children : [
+        { label : "login" , path : "/ngr-login" } ,
+        { divider : true },
+        { label : "admin" , path : "..." }
+      ]
+    }
+    ];
 
   constructor(public preferencesService : PreferencesService) { 
     //injection de dépendance
